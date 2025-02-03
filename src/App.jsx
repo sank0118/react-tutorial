382b783359d52df8da98a4ca0a1b47cf9565632f
sank0118 <tks9901@naver.com>
2025-02-03 13:37:41 +0900

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Counter from "./Counter";
+import Like from "./Like";
 
 const App = () => {
+  const [name, setName] = useState("강산");
+  const [count, setCount] = useState(0);
+  const [liked, setLiked] = useState(false);
+
   return (
-    <div>
-      <h1>React-Tutorial1</h1>
-    </div>
+    <>
+      <Header name={name} />
+      <h1>React-Tutorial</h1>
+      <input
+        type="text"
+        value={name}
+        placeholder="Enter Your Name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Counter count={count} setCount={setCount} />
+      <Like liked={liked} setLiked={setLiked} />
+      <Footer name={name} />
+    </>
   );
 };
 
---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = ({ name }) => {
+  return (
+    <footer>
+      <p>{name}의 페이지에 방문해주셔서 감사합니다. 안녕히 가세요.</p>
+    </footer>
+  );
+};
+
+export default Footer;
