2b1ef0e0158e29864ee477b5b4ed22be82fa23a9
sank0118 <tks9901@naver.com>
2025-02-11 10:08:06 +0900

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,103 @@
-import { useState, useEffect, useRef } from "react";
-
-const initialState = [];
+import PropTypes from "prop-types";
+import { useState } from "react";
 
 const App = () => {
-  const [todos, setTodos] = useState(initialState);
-  const [todo, setTodo] = useState("");
-
-  const ref = useRef(null);
-
-  const onSubmit = () => {
-    console.log(todo);
-    if (todo.length === 0) {
-      alert("경고");
-      return ref.current?.focus();
-    }
-
-    if (todo.length <= 1) {
-      alert("warning");
-      return ref.current?.focus();
-    }
-
-    // setTodos((prev) => {
-    //   return [todo, ...prev];
-    //   //set함수는 무조건 원래의 값과 같은 타입을 return 해줘야함
-    // }); //배열은 무조건 이전값을 다뤄야 함
-    setTodos((prev) => [todo, ...prev]);
-
-    setTodo("");
-    ref.current?.focus();
-  };
-
-  useEffect(() => {
-    console.log({ todos });
-  }, [todos]);
+  const [todos, setTodos] = useState(["Learn React", "Sudy more", "Cleaning"]);
 
   return (
     <div>
       <h1>App</h1>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          onSubmit();
-        }}
-      >
-        <input
-          type="text"
-          value={todo}
-          onChange={(e) => setTodo(e.target.value)}
-          ref={ref}
-        />
-        <button>ADD</button>
-      </form>
-      <ul>
-        {todos.map((todo) => {
-          const onDelete = () =>
-            setTodos((prev) => prev.filter((item) => item !== todo));
-
-          return (
-            <li key={todo}>
-              {todo}
-              <button onClick={onDelete}>삭제</button>
-            </li>
-          );
-        })}
-      </ul>
+      <TodoForm todos={todos} setTodos={setTodos} />
+      {todos.map((todo, index) => {
+        return (
+          <TodoItem
+            key={todo}
+            index={index}
+            todo={todo}
+            setTodos={setTodos}
+            todos={todos}
+          />
+        );
+      })}
     </div>
   );
 };
 
 export default App;
+
+const TodoItem = ({ todo, index, setTodos, todos }) => {
+  const onDelte = () => {
+    setTodos((prev) => prev.filter((item) => item !== todo));
+    alert("삭제되었습니다.");
+  };
+  return (
+    <>
+      <div style={{ display: "flex", columnGap: 10 }}>
+        <p>
+          {index + 1}. {todo}
+        </p>
+        <button onClick={onDelte}>삭제</button>
+      </div>
+      <TodoForm setTodos={setTodos} todos={todos} isUpadate payload={todo} />
+    </>
+  );
+};
+
+TodoItem.propTypes = {
+  todo: PropTypes.string,
+  index: PropTypes.number,
+  setTodos: PropTypes.func,
+  todos: PropTypes.arrayOf(PropTypes.string),
+};
+
+const TodoForm = ({ todos, setTodos, isUpadate, payload }) => {
+  const [input, setInput] = useState();
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (input.length === 0) {
+          return alert("type todos");
+        }
+        const foundIndex = todos.findIndex((i) => {
+          if (!isUpadate) {
+            return i === input && i;
+          }
+          return i === payload && i;
+        });
+        if (foundIndex >= 0) {
+          return alert("Already added");
+        }
+        if (isUpadate && foundIndex < 0) {
+          return alert("No Such Item");
+        }
+        setTodos((prev) => {
+          let copy = [...prev];
+          if (!isUpadate) {
+            copy.unshift(input);
+          } else {
+            copy[foundIndex] = input;
+          }
+
+          return copy;
+        });
+        alert("추가되었습니다.");
+      }}
+    >
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button>{isUpadate ? "수정" : "추가"}</button>
+    </form>
+  );
+};
+
+TodoForm.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.string),
+  setTodos: PropTypes.func,
+  isUpadate: PropTypes.bool,
+  payload: PropTypes.string,
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-// import App from "./App.jsx";
-import Todo from "./todo/Todo.jsx";
+import App from "./App.jsx";
+// import Todo from "./todo/Todo.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <App /> */}
-    <Todo />
+    <App />
+    {/* <Todo /> */}
   </StrictMode>
 );
