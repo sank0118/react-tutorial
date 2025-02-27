d879a5ea4a853a0ff54331d5081b471a46007e4a
sank0118 <tks9901@naver.com>
2025-02-27 15:19:06 +0900

    초기화

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import RContainer from "./r/RContainer";
-// import StudentApp from "./StudentApp";
-// import App from "./App";
+
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RContainer />
-    {/* <StudentApp /> */}
-    {/* <App /> */}
+    <App />
   </StrictMode>
 );
